fba35be8b7f5e51ae0f8562ed6ca7c3fbaede0ce
Axel Ibo <axelyvesi2@gmail.com>
2023-09-04 22:09:50 +0200

    Installation des dependances modulaire du projet

--- front-end-angular/src/app/core/core.module.ts
+++ front-end-angular/src/app/core/core.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from './component/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ], 
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
