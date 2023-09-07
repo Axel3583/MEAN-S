676b34b25f9d9255e1b37dd55716b1c7bb423a59
Axel Ibo <axelyvesi2@gmail.com>
2023-09-07 20:33:20 +0200

    CodeRiview de la logique métier du composant Login.

--- back-end-express-server/src/auth/auth.service.ts
+++ back-end-express-server/src/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { User } from './Schema/user.schema';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -12,10 +18,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async signUp(signUpDto: {
-    username: string;
-    password: string;
-  }): Promise<{ token: string }> {
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
     const { username, password } = signUpDto;
 
     try {
@@ -44,5 +47,24 @@ export class AuthService {
       // Gére les erreurs possibles ici (par exemple, erreurs de validation, erreurs de base de données)
       throw error;
     }
+  }
+
+  async login(loginDto: LoginDto): Promise<{ token: string }> {
+    const { username, password } = loginDto;
+    // Étape 1 : Recherchez l'utilisateur par nom d'utilisateur
+    const user = await this.userModel.findOne({ username });
+    // Étape 2 : Vérifiez si l'utilisateur existe
+    if (!user) {
+      throw new UnauthorizedException('Invalid username or password');
+    }
+    // Étape 3 : Vérifiez le mot de passe
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException('Invalid username or password');
+    }
+    // Étape 4 : Générez un jeton JWT
+    const token = this.jwtService.sign({ sub: user._id });
+    // Étape 5 : Retournez le jeton JWT pour une utilisation ultérieure
+    return { token };
   }
 }
